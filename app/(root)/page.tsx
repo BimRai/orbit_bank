92ca6a92cf6319e7ce5b457a086f3a5c63f7ca29
BimRai <bimitrai@icloud.com>
2024-06-13 13:59:44 +0100

    modified:   app/(root)/page.tsx 	new file:   components/AnimatedCounter.tsx 	new file:   components/DoughnutChart.tsx 	new file:   components/HeaderBox.tsx     new file:   components/TotalBalanceBox.tsx 	modified:   lib/utils.ts 	modified:   package-lock.json     updated dependencies   package.json

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 
 const Home = () => {
+  const loggedIn = { firstName: 'Bim' };
+
   return (
-    <div>Dashboard</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Overview and management of all your bank accounts and transactions"
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={2351.20}
+            
+          />
+        </header>
+      </div>
+    </section>
   )
 }
 
